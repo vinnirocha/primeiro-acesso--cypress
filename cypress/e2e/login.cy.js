e4714b11c52521a9c4823a1cf2c35ea4b59e20c5
Matteo Vinicius Rocha <matteorocha2204@gmail.com>
2025-07-16 17:01:48 -0300

    First commit

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,19 @@
+describe('Orange HRM tests', () => {
+it('login - success', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get("[name='username']").type('Admin') 
+    cy.get("[name='password']").type('admin123')
+    cy.get("[type='submit']").click()
+    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    cy.get('.oxd-topbar-header-breadcrumb-module').contains('Dashboard')
+ })
+it('login - fail', () => {
+
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get("[name='username']").type('test') 
+    cy.get("[name='password']").type('test')
+    cy.get("[type='submit']").click()
+    cy.get('.oxd-alert')
+
+  })
+})  
